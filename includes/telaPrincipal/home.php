6ad01dfc519e410117ec093bf601741a3d506349
AliceSilvah <allysilvaa2004@gmail.com>
2022-05-05 16:14:16 -0300

    atualização  home page

--- includes/telaPrincipal/home.php
+++ includes/telaPrincipal/home.php
@@ -13,6 +13,48 @@
     </head>
 
     <body>
+        <nav class="navbar navbar-expand-lg navbar-dark" id="nav" style=" background-color: rgb(75,0,130)">
+            <a class="navbar-brand" href="#">Nome</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Alterna navegação">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item ">
+                    <a class="nav-link" href="#">Plataforma</a>
+                </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="#"></a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="#">Clientes</a>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle"  href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Pesquisas
+            </a>
+
+            <div class="dropdown-menu"  aria-labelledby="navbarDropdownMenuLink">
+              <h6 class="dropdown-header">Tipos de pesquisa</h6>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Pesquisa NPS</a>
+              <a class="dropdown-item" href="#">Pesquisa CES</a>
+              <a class="dropdown-item" href="#">Pesquisa CSAT</a>  
+            </div>
+          </li>
+        </ul>
+
+        <div class="ladinho">
+          <button type="button" class="btn btn-outline-light  mr-3 ">Login</button> 
+          <button type="button" class="btn btn-outline-light">Cadastre-se</button>
+        </div><!--fim ladinho-->
+      </div><!--fim collapse navbar-collapse-->
+    </nav><!--fim nav-->
+
 
 
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
