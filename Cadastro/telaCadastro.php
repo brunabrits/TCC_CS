9decddb85f55526fedf86821b57cee598cd1aadf
Dandara <dandara.melo01@etec.sp.gov.br>
2022-05-20 17:32:42 -0300

    estilização cadastro

--- Cadastro/telaCadastro.php
+++ Cadastro/telaCadastro.php
@@ -3,33 +3,30 @@
 <head>
 	<meta charset="utf-8">
 	<title> Sistema de Cadastro </title>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="css/style.css"/>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
 <header></header>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">TCC</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#">Cliente</a>
-        <a class="nav-link" href="#">Contatos</a>
-        <a class="nav-link" href="#">Dados da Pesquisa</a>
-		<a class="nav-link" href="#">Empresas</a>
-        <a class="nav-link" href="#">Endereço</a>
-        <a class="nav-link" href="#">Funcionários</a>
-		<a class="nav-link" href="#">Pesquisas</a>
-        <a class="nav-link" href="#">Produto</a>
-        <a class="nav-link" href="#">Relatórios</a>
+    <nav class="navbar navbar-expand-lg navbar-dark" id="nav" style=" background-color: rgb(75,0,130)">
+      <a class="navbar-brand" href="#">Nome</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Alterna navegação">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item ">
+            <a class="nav-link" href="#">Plataforma</a>
+          </li>
+
+        </ul>
+       </div>
       </div>
-    </div>
-  </div>
-</nav>
+    </nav>
 <div class="container">
 	<br>
 	<h1>Sistema de Cadastro</h1>
