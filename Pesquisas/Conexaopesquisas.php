caa0065480bc232e645fd10c09397ea21e7ff32a
brunabrits <brunabsilva0409@gmail.com>
2022-05-20 22:34:07 -0300

    Conexao do banco das pesquisas

---
+++ Pesquisas/Conexaopesquisas.php
@@ -0,0 +1,18 @@
+<?php
+
+class Conexao
+{
+    private static $instancia;
+
+    public static function getConexao()
+    {
+        if(!isset(self::$instancia)){
+            self::$instancia = new PDO('mysql:host=localhost; dbname=metricas; charset=utf8', 'root', '');
+            return self::$instancia;
+        }else{
+            return self::$instancia;
+        }	
+    }
+}
+
+?>
