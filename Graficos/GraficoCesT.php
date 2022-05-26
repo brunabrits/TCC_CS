3f46c3d6535d3255e4b731acce51d8a6b6e88d36
Lucas Chaves <lucas.nascimento269@etec.sp.gov.br>
2022-05-26 15:15:59 -0300

    Teste para apresentar

---
+++ Graficos/GraficoCesT.php
@@ -0,0 +1,30 @@
+<html>
+  <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Month');
+        data.addColumn('number', 'Sales');
+        data.addRows([
+          ['Facil',  7],
+          ['Dificil',2],
+          ['Medio',  1]
+        ]);
+
+        var options = {
+          title: 'Grafico CES',
+          pieHole: 0.4,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  <body>
+    <div id="donutchart" style="width: 900px; height: 500px;"></div>
+  </body>
+</html>
