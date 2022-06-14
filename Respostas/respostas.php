7edd2b26b04527e82a21f52c376599fb711ce134
Silas Felipe <silasfelipe.5089@gmmail.com>
2022-06-14 17:29:34 -0300

    Painel de Respostas

---
+++ Respostas/respostas.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+    <!--Link das fontes Material Icons-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+
+    <title>Meus Clientes</title>
+</head>
+
+<body>
+
+<nav>
+    <div class="nav-wrapper purple darken-4">
+      <a href="#" class="brand-logo center">Meus Clientes</a>
+      
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+
+        <li><a href="../includes/telaLogin/painelTeste.php">Home</a></li>
+        <li><a href="badges.html">Dashboards</a></li>
+        
+      </ul>
+
+    </div>
+  </nav>
+</body>
+
+</html>
