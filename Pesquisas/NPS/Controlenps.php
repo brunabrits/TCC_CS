9de7b32246e8acdfac77e4b9ba1befde43330662
brunabrits <brunabsilva0409@gmail.com>
2022-06-17 03:33:30 -0300

    feat: 🛠

--- Pesquisas/NPS/Controlenps.php
+++ Pesquisas/NPS/Controlenps.php
@@ -3,14 +3,13 @@
 include 'En_pesquisanps.php'; // esse é tipo o clienteDao com as configurações do banco 
 include 'ValNps.php'; // esse seria o cliente, com os gets e sets
 
-
 $nomenps =$_GET['nomenps'];
 $dtnps =$_GET['dtnps'];
 $emainps =$_GET['emainps'];
 $nps =$_GET['nps'];
 $botao = $_GET['botao'];
 
-$Valnps = new ValNps(); // era teste 
+$Valnps = new ValNps(); 
 $En_pesquisanps = new En_pesquisa();
 
 $Valnps->setNomenps($nomenps);
@@ -21,4 +20,5 @@ $Valnps->setNps($nps);
 if($botao=="Enviar"){
    $En_pesquisanps->incluir($Valnps);
 }
+
 ?>
--- Pesquisas/NPS/En_pesquisanps.php
+++ Pesquisas/NPS/En_pesquisanps.php
@@ -1,27 +1,56 @@
 <?php 
-// Aqui ele envia as respostas para o banco 
-include '../Conexaopesquisas.php';
+include '../Database/conexao.php';
 
 class En_pesquisa
 {
-	public function incluir(ValNps $n){
-		                  // era o mauisculo Teste 
-		
-		$sql = "insert into nps values (?, ?, ?, ?)";
+	public function incluir(ValNps  $n){
+  
+		$sql = "INSERT INTO Respostas(Vlr_resposta, Email_resposta,Dt_nasc) VALUES (?,?,?)";
 		$bd = new Conexao();
 		$con = $bd->getConexao();
 		$stm = $con->prepare($sql);
-		$stm->bindValue(1, $n->getNomenps());
-		$stm->bindValue(2, $n->getDtnps());
-		$stm->bindValue(3, $n->getEmainps());
-		$stm->bindValue(4, $n->getNps());
+		$stm->bindValue(1, $n->getNps());
+		$stm->bindValue(2, $n->getEmainps());
+		$stm->bindValue(3, $n->getDtnps());
 		$resultado = $stm->execute();
 
-		if($resultado){
-			echo "Pesquisa Enviada";
-		}else{
-			echo "Não foi possível concluir";
-		}
+		$nps =$_GET['nps'];
+
+			if ($nps<=6)
+			{
+			   $sql_d = "INSERT INTO Nps(Tp_nps_detrator) VALUES (1)";
+			   $vlr = $con->prepare($sql_d);
+			}
+			
+			else if ($nps ==7 or $nps ==8)
+			{
+			   $sql_n = "INSERT INTO Nps(Tp_nps_neutro) VALUES (1)";
+			   $vlr = $con->prepare($sql_n);
+			}
+			
+			else if ($nps ==9 or  $nps ==10)
+			{
+			   $sql_p = "INSERT INTO Nps(Tp_nps_promotor) VALUES (1)";
+			   $vlr = $con->prepare($sql_p);
+			}
+			else
+			{
+			   echo("ERRO NPS ");
+			}
+		
+			$vlr_resultado = $vlr->execute();
+
+			if($resultado){
+			 ?>
+				<script> alert('Sua resposta foi enviada com Sucesso!')
+				javascript:window.location='PesquisaNps.php'
+				</script>
+				<?php }else{
+					echo "Não foi possível concluir";
+   			}
+	
+
 	}
 }
+
 ?>
