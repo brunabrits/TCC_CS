ee2aac8eb3c7b9fbe24ed51ddbbb8f0b7cc1eeb9
brunabrits <brunabsilva0409@gmail.com>
2022-06-17 05:10:59 -0300

    fix: 🩹

--- Pesquisas/Conexaopesquisas.php
+++ Pesquisas/Conexaopesquisas.php
@@ -7,7 +7,7 @@ class Conexao
     public static function getConexao()
     {
         if(!isset(self::$instancia)){
-            self::$instancia = new PDO('mysql:host=localhost; dbname=metricas; charset=utf8', 'root', '');
+            self::$instancia = new PDO('mysql:host=localhost; dbname=Tcc_cs; charset=utf8', 'root', '');
             return self::$instancia;
         }else{
             return self::$instancia;
