58236eb6a1a4dd57d3cafe6b2bf1ccc41d9993a8
brunabrits <brunabsilva0409@gmail.com>
2022-06-17 04:41:03 -0300

    test: 🧪

--- Pesquisas/NPS/En_pesquisanps.php
+++ Pesquisas/NPS/En_pesquisanps.php
@@ -40,6 +40,11 @@ class En_pesquisa
 		
 			$vlr_resultado = $vlr->execute();
 
+			$sql_pes = "INSERT INTO Pesquisas(Tp_pesquisa) VALUES ('NPS')";
+			$pes = $con->prepare($sql_pes);
+			$Tp_pes = $pes->execute();
+
+
 			if($resultado){
 			 ?>
 				<script> alert('Sua resposta foi enviada com Sucesso!')
