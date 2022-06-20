a374fc108feae7e840a6aa4cdc8e6fc4a268d422
Bruna Brito <Brunabsilva0409@gmail.com>
2022-06-20 15:49:59 -0300

    hotfix: 🚑

--- tcc_cs.sql
+++ tcc_cs.sql
@@ -1,3 +1,4 @@
+
 create database Tcc_cs;
 
 use Tcc_cs;
@@ -9,10 +10,6 @@ create table Cliente(
     Senha_cliente 	varchar(32) not null
 );
 
-create table Pesquisas(
-	Id_pesquisa 	int primary key auto_increment,
-    Tp_pesquisa 	varchar(45) not null
-);
 
 create table Nps(
 	Id_nps 					int primary key auto_increment,
@@ -31,6 +28,7 @@ create table Csat(
     Tp_csat_mti		int(1)  null
 );
 
+
 create table Ces(
 	Id_ces 		int primary key auto_increment,
     Tp_ces_mf 	int(1)  null,
@@ -40,25 +38,17 @@ create table Ces(
     Tp_ces_md 	int(1)  null
 );
 
-create table Metricas(
-	Id_metricas 	int primary key auto_increment,
-    Nm_metricas 	varchar(10) not null,
-    fk_Id_nps 		int null,
+
+create table Perguntas(
+	Id_pergunta			int not null primary key,
+    Nm_pergunta 		varchar(100) null,
+	fk_Id_nps 		int null,
     fk_Id_csat 		int null,
     fk_Id_ces		int null,
 	foreign key 	(fk_Id_nps) 	references 	Nps (Id_nps),
     foreign key 	(fk_Id_csat) 	references 	Csat (Id_csat),
     foreign key 	(fk_Id_ces) 	references 	Ces (Id_ces)
-);
 
-create table Perguntas(
-	Id_pergunta			int not null primary key,
-    Nm_pergunta 		varchar(100) not null,
-    Ds_pergunta 		varchar(100) not null,
-    fk_Id_metricas 		int not null,
-    fk_Id_pesquisas 	int not null,
-    foreign key 		(fk_Id_metricas)	references 	Metricas  (Id_metricas),
-    foreign key 		(fk_Id_pesquisas)	references 	Pesquisas (Id_pesquisa)
 );
 
 create table Respostas(
@@ -66,18 +56,11 @@ create table Respostas(
     Vlr_resposta 		int not null,
     Email_resposta 		varchar(50) not null,
     Dt_nasc             Date not null,
-	fk_Id_pergunta 		int,
-    foreign key 		(fk_Id_pergunta) references Perguntas (Id_pergunta)
+    Id_pesquisa 		int null,
+    Tp_pesquisa 	    varchar(45) null
+	
 );    
 
-show tables;
 
-desc Cliente;
-desc Pesquisas;
-desc Nps;
-desc Csat;
-desc Ces;
-desc Metricas;
-desc Perguntas;
-desc Respostas;
+
 
