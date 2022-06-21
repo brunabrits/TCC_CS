4de5322910c360bdf3109f1b7b9cfce160295b1e
Bruna Brito <81496282+brunabrits@users.noreply.github.com>
2022-06-21 08:21:15 -0300

    fix: 🩹

--- tcc_cs.sql
+++ tcc_cs.sql
@@ -4,30 +4,30 @@ create database Tcc_cs;
 use Tcc_cs;
 
 create table Cliente(
-	Id_cliente 		int primary key auto_increment,
+    Id_cliente 		int primary key auto_increment,
     Nm_cliente 		varchar(70) not null,
     Email_cliente 	varchar(50) unique not null,
     Senha_cliente 	varchar(32) not null
 );
 
 create table Pesquisa(
- Id_pesquisa 		int auto_increment,
-Tp_pesquisa 	    varchar(4) null
+ Id_pesquisa 	    int  primary key auto_increment,
+ Tp_pesquisa 	    varchar(4) null
 );
 
 
 create table Nps(
-	Id_nps 					int primary key auto_increment,
+    Id_nps 			int primary key auto_increment,
     Tp_nps_promotor 		int(2)  null,
-    Tp_nps_neutro 			int(1)  null,
+    Tp_nps_neutro 		int(1)  null,
     Tp_nps_detrator 		int(1)  null
 );
 
 
 create table Csat(
-	Id_csat 		int primary key auto_increment,
+    Id_csat 		int primary key auto_increment,
     Tp_csat_mts		int(1)  null,
-	Tp_csat_s 		int(1)  null,
+    Tp_csat_s 		int(1)  null,
     Tp_csat_n 		int(1)  null,
     Tp_csat_i 		int(1)  null,
     Tp_csat_mti		int(1)  null
@@ -35,7 +35,7 @@ create table Csat(
 
 
 create table Ces(
-	Id_ces 		int primary key auto_increment,
+    Id_ces 	int primary key auto_increment,
     Tp_ces_mf 	int(1)  null,
     Tp_ces_f 	int(1)  null,
     Tp_ces_n 	int(1)  null,
@@ -45,15 +45,15 @@ create table Ces(
 
 
 create table Perguntas(
-Id_pergunta		int not null primary key,
-Vlr_pergunta		int(1) not null, 		
-Nm_pergunta 		varchar(100) null,
-fk_Id_nps 		int null,
-fk_Id_csat 		int null,
-fk_Id_ces		int null,
-    foreign key 	(fk_Id_nps) 	references 	Nps (Id_nps),
-    foreign key 	(fk_Id_csat) 	references 	Csat (Id_csat),
-    foreign key 	(fk_Id_ces) 	references 	Ces (Id_ces)
+	Id_pergunta		int not null primary key auto_increment,
+	Vlr_pergunta		int(1) not null, 		
+	Nm_pergunta 		varchar(100) null,
+	fk_Id_nps 		int null,
+	fk_Id_csat 		int null,
+	fk_Id_ces		int null,
+    	foreign key 	(fk_Id_nps) 	references 	Nps (Id_nps),
+    	foreign key 	(fk_Id_csat) 	references 	Csat (Id_csat),
+    	foreign key 	(fk_Id_ces) 	references 	Ces (Id_ces)
 
 );
 
@@ -61,7 +61,7 @@ create table Respostas(
     Id_resposta 		int not null primary key auto_increment,
     Vlr_resposta 		int not null,
     Email_resposta 		varchar(50) not null,
-    Dt_nasc             Date not null
+    Dt_nasc                     Date not null
 	
 );    
 
