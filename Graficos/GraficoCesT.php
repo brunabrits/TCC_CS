f930f0d619ce1dc37ae995daa08c0413c4b9b7d4
Dandara <dandara.melo01@etec.sp.gov.br>
2022-06-23 12:38:40 -0300

    gráficos estilizados

--- Graficos/GraficoCesT.php
+++ Graficos/GraficoCesT.php
@@ -1,5 +1,6 @@
 <html>
   <head>
+    <link rel="stylesheet" type="text/css" href="css/graficoCesT.css"/>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
       google.charts.load("current", {packages:["corechart"]});
@@ -25,6 +26,48 @@
     </script>
   </head>
   <body>
+  <main>
+   <h1 class="title">Gráfico CES</h1>
+   
+   <div class="info-data">
+     <div class="card">
+       <div class="head">
+         <div>
+            <h2>70%</h2>
+            <p>Fácil</p>
+            <p>Os clientes promotores são os consumidores que
+               sua empresa conseguiu fidelizar e que agem como verdadeiros propagadores do seu negócio.</p>
+         </div>
+         <i class="bx-bx-trending-up"></i>
+        </div> 
+       
+     </div> 
+     <div class="card">
+       <div class="head">
+         <div>
+            <h2>20%</h2>
+            <p>Difícil</p>
+            <p>Eles não estão propensos a fazer reclamações sobre a marca. Apesar disso, eles possivelmente também não serão consumidores frequentes.</p>
+         </div>
+         <i class="bx-bx-trending-up"></i>
+        </div> 
+        
+     </div> 
+     <div class="card">
+       <div class="head">
+         <div>
+            <h2>10%</h2>
+            <p>Médio</p>
+            <p>O cliente detrator é o consumidor que teve uma experiência ruim com uma empresa e possivelmente fará reclamações.</p>
+         </div>
+         <i class="bx-bx-trending-up"></i>
+        </div> 
+        
+     </div> 
+   </div>
+   </main>
+   <div class="cardg">
     <div id="donutchart" style="width: 900px; height: 500px;"></div>
+   </div> 
   </body>
 </html>
--- Graficos/GraficoCsatT.php
+++ Graficos/GraficoCsatT.php
@@ -1,4 +1,5 @@
 <head>
+    <link rel="stylesheet" type="text/css" href="css/graficoCsatT.css"/>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
       google.charts.load('current', {'packages':['corechart']});
@@ -8,15 +9,15 @@
 
         var data = google.visualization.arrayToDataTable([
           ['Task', 'Hours per Day'],
-          ['Pouco Satisfeito',      1],
-          ['Muito Isatisfeito',     6],
+          ['Insatisfeito',      1],
+          ['Muito Insatisfeito',     6],
           ['Indiferente',           1],
           ['Muito Satisfeito',      1],
-          ['Pouco Insatisfeito',    1]
+          ['Satisfeito',    1]
         ]);
 
         var options = {
-          title: 'Grafico CSAT'
+          title: 'Gráfico CSAT'
         };
 
         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
@@ -26,6 +27,80 @@
     </script>
   </head>
   <body>
+  <main>
+
+<h1 class="title">Gráfico CSAT</h1>
+<!--<ul class="breadcrumbs">
+   <li><a href="#">Home</a></li>
+   <li class="divider"></li>
+   <li><a href="#" class="active">Grafico NPS</a></li>
+</ul>-->
+<div class="info-data">
+  <div class="card">
+    <div class="head">
+      <div>
+         <h2>60%</h2>
+         <p>Muito Insatisfeito</p>
+         <p>Os clientes promotores são os consumidores que
+            sua empresa conseguiu fidelizar e que agem como verdadeiros propagadores do seu negócio.</p>
+      </div>
+      <i class="bx-bx-trending-up"></i>
+     </div> 
+     <!-- <span class="progress" data-value="50"></span>
+     <span class="label">50%</span> -->
+  </div> 
+  <div class="card">
+    <div class="head">
+      <div>
+         <h2>10%</h2>
+         <p>Insatisfeito</p>
+         <p>Eles não estão propensos a fazer reclamações sobre a marca. Apesar disso, eles possivelmente também não serão consumidores frequentes.</p>
+      </div>
+      <i class="bx-bx-trending-up"></i>
+     </div> 
+     <!-- <span class="progress" data-value="50"></span>
+     <span class="label">50%</span> -->
+  </div> 
+  <div class="card">
+    <div class="head">
+      <div>
+         <h2>10%</h2>
+         <p>Indiferente</p>
+         <p>O cliente detrator é o consumidor que teve uma experiência ruim com uma empresa e possivelmente fará reclamações.</p>
+      </div>
+      <i class="bx-bx-trending-up"></i>
+     </div> 
+     <!-- <span class="progress" data-value="50"></span>
+     <span class="label">50%</span> -->
+  </div> 
+  <div class="card">
+    <div class="head">
+      <div>
+         <h2>10%</h2>
+         <p>Satisfeito</p>
+         <p>O cliente detrator é o consumidor que teve uma experiência ruim com uma empresa e possivelmente fará reclamações.</p>
+      </div>
+      <i class="bx-bx-trending-up"></i>
+     </div> 
+     <!-- <span class="progress" data-value="50"></span>
+     <span class="label">50%</span> -->
+  </div> 
+  <div class="card">
+    <div class="head">
+      <div>
+         <h2>10%</h2>
+         <p>Muito Satisfeito</p>
+         <p>O cliente detrator é o consumidor que teve uma experiência ruim com uma empresa e possivelmente fará reclamações.</p>
+      </div>
+      <i class="bx-bx-trending-up"></i>
+     </div> 
+     <!-- <span class="progress" data-value="50"></span>
+     <span class="label">50%</span> -->
+  </div> 
+</div>
+</main>
+<div class="cardg">
     <div id="piechart" style="width: 900px; height: 500px;"></div>
+</div>  
   </body>
 </html>
