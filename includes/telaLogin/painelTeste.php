1d3f6292dbac0c9f54a62ef7c10e6b3023ac7a8f
Bruna Brito <Brunabsilva0409@gmail.com>
2022-06-23 14:07:45 -0300

    style: 🖌

--- includes/telaLogin/painelTeste.php
+++ includes/telaLogin/painelTeste.php
@@ -1,4 +1,4 @@
-    <?php
+<?php
         session_start();
 
         if( (!isset($_SESSION['Usuario']) == true) and (!isset($_SESSION['Chave']) == true) )
@@ -10,10 +10,11 @@
         }
         $Logado = ($_SESSION['Usuario']);
     ?>
+
 <!DOCTYPE html>
-<html lang="pt-br">
-<head>
-   
+<!-- Created by CodingLab |www.youtube.com/CodingLabYT-->
+<html lang="en" dir="ltr">
+  <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -21,285 +22,353 @@
     <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-    <title>Painel</title>
-    <style>
-*{
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-            /* text-decoration: none; */
-            font-family: Arial, Helvetica, sans-serif;
-        }
-
-
-
-
-        .sidebar{
-            position: fixed;
-            top: 0;
-            left: 0;
-            height: 100%;
-            width: 260px;
-            background:  rgb(75,0,130);
-
-        }
-
-        .sidebar .logo-details{
-            height: 65px;
-            width: 100%;
-            display: flex;
-            align-items: center;
-
-        }
-
-        .sidebar .logo-details i{
-            font-size: 30px;
-            color: #fff;
-            
-            height: 50px;
-            min-width: 78px;
-            text-align: center;
-            line-height: 50px;
-
-        }
-
-        .sidebar .logo-details .logo_name{
-            font-size: 22px;
-            color: #fff;
-            font-weight: 600;
-        }
-
-        .sidebar .nav-links{
-            background: rgb(75,0,130);
-            height: 100%;
-            padding-top: 30px 0 150px 0;
-            overflow: auto;
-        }
-
-        .sidebar .nav-links::-webkit-scrollbar{
-            display: none;
-        }
-       
-        .sidebar .nav-links li{
-            list-style: none;
-            position: relative;
-            transition: all 0.4s ease;
-
-        }
-
-        .sidebar .nav-links li:hover{
-            background: #ceaadb;
-        }
-
-
-        .sidebar .nav-links li .iocn-link{
-           display: flex; 
-           align-items: center;
-           justify-content: space-between;
-        }
-
-
-        .sidebar .nav-links li i{
-            height: 50px;
-            min-width: 66px;
-            text-align: center;
-            line-height: 50px;
-            color: #fff;
-            font-size: 20px;
-            transition: all 0.3s ease;
-        }
-
-        .sidebar .nav-links li.showMenu i.arrow{
-            transform: rotate(-180deg);
-        }
-
-        .sidebar .nav-links li a{
-            text-decoration: none;
-            align-items: center;
-            display: flex;
-        }
-
-        .sidebar .nav-links li a .link_name{
-            font-size: 18px;
-            font-weight: 400;
-            color: #fff;
-        }
-
-        .sidebar .nav-links li .sub-menu{
-            padding: 6px 6px 14px 80px;
-            margin-top: -10px;
-            background: #ceaadb;
-            display: none; 
-        }
-
-        .sidebar .nav-links li.showMenu .sub-menu{
-            display: block;  
-        }
-
-        .sidebar .nav-links li .sub-menu a{
-            color: #fff;
-            font-size: 15px;
-            padding: 5px 0;
-            white-space: nowrap;
-            opacity: 0.6;
-            transition: all 0.3 ease;
-        }
-
-        .sidebar .nav-links li .sub-menu a:hover{
-            opacity: 1;
-        }
-
-        .sidebar.close .nav-links li .sub-menu{
-           position: absolute;
-           left: 100%;
-           top: -10px;
-           margin-top: 0;
-           padding: 10px 20px;
-           border-radius: 0 6px 6px 0;
-           transition: all 0.4s ease;
-           opacity: 0;
-           pointer-events: none;
-        }
-
-        
-
-        .sidebar.close .nav-links li:hover .sub-menu{
-            top: 0;
-            opacity: 1;
-            pointer-events: auto;
-
-        }
-
-        .sidebar .nav-links li .sub-menu .link_name{
-            display: none ;
-
-        }
-
-
-        .sidebar.close .nav-links li .sub-menu .link_name{
-           font-size: 18px;
-           opacity: 1;
-           display: block;
-        }
-
-        .home-section{
-            position: relative;
-            background: #E4E9F7;
-            height: 100vh;
-            left: 260px;
-            width: calc(100% - 260px)
-        }
-
-        .home-section .home-content{
-            height: 60px;
-            display: flex;
-            align-items: center;
-        }
-
-        .home-section .home-content .bx-menu,
-        .home-section .home-content .text{
-            color: #11101d;
-            font-size: 35px;
-        }
-
-        .home-section .home-content .bx-menu{
-            margin: 0 15px;
-        }
-
-        .home-section .home-content .text{
-            font-size: 26px;
-            font-weight: 600;
-        }
-    </style>
-</head>
-
-    <div class="sidebar">
-        <div class="logo-details">
-           <i class='bx bxs-face-mask'></i>
-           <span class="logo_name">Nome</span>
-        </div>
-        <ul class="nav-links">
-
-            <li>
-                <a href="#">
-                <i class='bx bxs-user-plus' ></i>
-                  <span class="link_name">Clientes</span>
-                </a>
-            </li>
-
-            <li>
-                <div class="iocn-link">
-                  <a href="#">
-                    <i class='bx bx-signal-4'></i>
-                    <span class="link_name">Gráficos</span>
-                  </a>
-                  <i class='bx bxs-chevron-down arrow'></i>
-                </div>
-                <ul class="sub-menu">
-                    <li><a href="#">Gráfico NPS</a></li>
-                    <li><a href="#">Gráfico CES</a></li>
-                    <li><a href="#">Gráfico CSAT</a></li>
-                </ul>
-            </li>
-
-            <li>
-                <div class="iocn-link">
-                  <a href="#">
-                    <i class='bx bx-edit-alt'></i>
-                    <span class="link_name">Pesquisas</span>
-                  </a>
-                    <i class='bx bxs-chevron-down arrow'></i>
-                </div>
-                <ul class="sub-menu">
-                    <li><a href="../../TCC_CS/Pesquisas/NPS/PesquisaNps.php">Pesquisa NPS</a></li>
-                    <li><a href="../../TCC_CS/Pesquisas/CES/PesquisaCes.php">Pesquisa CES</a></li>
-                    <li><a href="../../TCC_CS/Pesquisas/CSAT/PesquisaCsat.php">pesquisa CSAT</a></li>
-                </ul>
-            </li>
-            
-            <li>
-                <div class="icon-link">
-                    <a href="logout.php">
-                        <i class="fa-solid fa-door-open"></i>
-                        <span class="link_name">Sair</span>
-                    </a>
-                </div>
-            </li>
-        </ul>
-    </div>
-
-    <section class="home-section">
-        <div class="home-content">
-           <i class='bx bx-menu'></i>
-           <span class="text">Drop Dows Sidebar</span>
-        </div>
-    </section>
-
-<script>
-    let arrow = document.querySelectorAll(".arrow");
-    for (var i = 0; i < arrow.length; i++) {
-        arrow[i].addEventListener("click", (e)=>{
-
-            let arrowParent = e.target.parentElement.parentElement;
-            console.log(arrowParent);
-            arrowParent.classList.toggle("showMenu");
-       });
-    }
-
-
-    let sidebar = document.querySelector(".sidebar");
-    let sidebarBtn = document.querySelector(".bx-menu");
-    console.log(sidebarBtn);
-    sidebarBtn.addEventListener("click", ()={
-        sidebar.classList.toggle("close");
-    });
-
-</script>
-
-
+    <!--<title> Drop Down Sidebar Menu | CodingLab </title>-->
     
-          <script src="script.js"></script>
-          <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-          <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <!-- Boxiocns CDN Link -->
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+   <style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Poppins', sans-serif;
+}
+.sidebar{
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 260px;
+  background: rgb(75,0,130);
+  z-index: 100;
+  transition: all 0.5s ease;
+}
+.sidebar.close{
+  width: 78px;
+}
+.sidebar .logo-details{
+  height: 60px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+}
+.sidebar .logo-details i{
+  font-size: 30px;
+  color: #fff;
+  height: 50px;
+  min-width: 78px;
+  text-align: center;
+  line-height: 50px;
+}
+.sidebar .logo-details .logo_name{
+  font-size: 22px;
+  color: #fff;
+  font-weight: 600;
+  transition: 0.3s ease;
+  transition-delay: 0.1s;
+}
+.sidebar.close .logo-details .logo_name{
+  transition-delay: 0s;
+  opacity: 0;
+  pointer-events: none;
+}
+.sidebar .nav-links{
+  height: 100%;
+  padding: 30px 0 150px 0;
+  overflow: auto;
+}
+.sidebar.close .nav-links{
+  overflow: visible;
+}
+.sidebar .nav-links::-webkit-scrollbar{
+  display: none;
+}
+.sidebar .nav-links li{
+  position: relative;
+  list-style: none;
+  transition: all 0.4s ease;
+}
+.sidebar .nav-links li:hover{
+  background: #1d1b31;
+}
+.sidebar .nav-links li .iocn-link{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.sidebar.close .nav-links li .iocn-link{
+  display: block
+}
+.sidebar .nav-links li i{
+  height: 50px;
+  min-width: 78px;
+  text-align: center;
+  line-height: 50px;
+  color: #fff;
+  font-size: 20px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+}
+.sidebar .nav-links li.showMenu i.arrow{
+  transform: rotate(-180deg);
+}
+.sidebar.close .nav-links i.arrow{
+  display: none;
+}
+.sidebar .nav-links li a{
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+}
+.sidebar .nav-links li a .link_name{
+  font-size: 18px;
+  font-weight: 400;
+  color: #fff;
+  transition: all 0.4s ease;
+}
+.sidebar.close .nav-links li a .link_name{
+  opacity: 0;
+  pointer-events: none;
+}
+.sidebar .nav-links li .sub-menu{
+  padding: 6px 6px 14px 80px;
+  margin-top: -10px;
+  background: #1d1b31;
+  display: none;
+}
+.sidebar .nav-links li.showMenu .sub-menu{
+  display: block;
+}
+.sidebar .nav-links li .sub-menu a{
+  color: #fff;
+  font-size: 15px;
+  padding: 5px 0;
+  white-space: nowrap;
+  opacity: 0.6;
+  transition: all 0.3s ease;
+}
+.sidebar .nav-links li .sub-menu a:hover{
+  opacity: 1;
+}
+.sidebar.close .nav-links li .sub-menu{
+  position: absolute;
+  left: 100%;
+  top: -10px;
+  margin-top: 0;
+  padding: 10px 20px;
+  border-radius: 0 6px 6px 0;
+  opacity: 0;
+  display: block;
+  pointer-events: none;
+  transition: 0s;
+}
+.sidebar.close .nav-links li:hover .sub-menu{
+  top: 0;
+  opacity: 1;
+  pointer-events: auto;
+  transition: all 0.4s ease;
+}
+.sidebar .nav-links li .sub-menu .link_name{
+  display: none;
+}
+.sidebar.close .nav-links li .sub-menu .link_name{
+  font-size: 18px;
+  opacity: 1;
+  display: block;
+}
+.sidebar .nav-links li .sub-menu.blank{
+  opacity: 1;
+  pointer-events: auto;
+  padding: 3px 20px 6px 16px;
+  opacity: 0;
+  pointer-events: none;
+}
+.sidebar .nav-links li:hover .sub-menu.blank{
+  top: 50%;
+  transform: translateY(-50%);
+}
+.sidebar .profile-details{
+  position: fixed;
+  bottom: 0;
+  width: 260px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background: #1d1b31;
+  padding: 12px 0;
+  transition: all 0.5s ease;
+}
+.sidebar.close .profile-details{
+  background: none;
+}
+.sidebar.close .profile-details{
+  width: 78px;
+}
+.sidebar .profile-details .profile-content{
+  display: flex;
+  align-items: center;
+}
+.sidebar .profile-details img{
+  height: 52px;
+  width: 52px;
+  object-fit: cover;
+  border-radius: 16px;
+  margin: 0 14px 0 12px;
+  background: #1d1b31;
+  transition: all 0.5s ease;
+}
+.sidebar.close .profile-details img{
+  padding: 10px;
+}
+.sidebar .profile-details .profile_name,
+.sidebar .profile-details .job{
+  color: #fff;
+  font-size: 18px;
+  font-weight: 500;
+  white-space: nowrap;
+}
+.sidebar.close .profile-details i,
+.sidebar.close .profile-details .profile_name,
+.sidebar.close .profile-details .job{
+  display: none;
+}
+.sidebar .profile-details .job{
+  font-size: 12px;
+}
+.home-section{
+  position: relative;
+  background: #E4E9F7;
+  height: 100vh;
+  left: 260px;
+  width: calc(100% - 260px);
+  transition: all 0.5s ease;
+}
+.sidebar.close ~ .home-section{
+  left: 78px;
+  width: calc(100% - 78px);
+}
+.home-section .home-content{
+  height: 60px;
+  display: flex;
+  align-items: center;
+}
+.home-section .home-content .bx-menu,
+.home-section .home-content .text{
+  color: #11101d;
+  font-size: 35px;
+}
+.home-section .home-content .bx-menu{
+  margin: 0 15px;
+  cursor: pointer;
+}
+.home-section .home-content .text{
+  font-size: 26px;
+  font-weight: 600;
+}
+@media (max-width: 400px) {
+  .sidebar.close .nav-links li .sub-menu{
+    display: none;
+  }
+  .sidebar{
+    width: 78px;
+  }
+  .sidebar.close{
+    width: 0;
+  }
+  .home-section{
+    left: 78px;
+    width: calc(100% - 78px);
+    z-index: 100;
+  }
+  .sidebar.close ~ .home-section{
+    width: 100%;
+    left: 0;
+  }
+}
+
+   </style>
+<body>
+
+  <div class="sidebar close">
+    <div class="logo-details">
+      <i class='bx bxl-c-plus-plus'></i>
+      <span class="logo_name">Customer Success</span>
+    </div>
+    <ul class="nav-links">
+      <li>
+        <a href="../../Respostas/listagem.php">
+          <i class='bx bx-grid-alt' ></i>
+          <span class="link_name">Meus Clientes</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">Meus Clientes</a></li>
+        </ul>
+      </li>
+      <li>
+        <div class="iocn-link">
+          <a href="#">
+            <i class='bx bx-pie-chart-alt-2' ></i>
+            <span class="link_name">Gráficos</span>
+          </a>
+          <i class='bx bxs-chevron-down arrow' ></i>
+        </div>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Gráficos</a></li>
+          <li><a href="../../graficos/graficoNps.php">NPS</a></li>
+          <li><a href="../../graficos/graficoCsat.php">CSAT</a></li>
+          <li><a href="../../graficos/graficoCes.php">CES</a></li>
+        </ul>
+      </li>
+      <li>
+        <div class="iocn-link">
+          <a href="#">
+            <i class='bx bx-book-alt' ></i>
+            <span class="link_name">Pesquisas existentes</span>
+          </a>
+          <i class='bx bxs-chevron-down arrow' ></i>
+        </div>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Pesquisas existentes</a></li>
+          <li><a href="../../Pesquisas/NPS/PesquisaNps.php">NPS</a></li>
+          <li><a href="../../Pesquisas/CSAT/PesquisaCsat.php">CSAT</a></li>
+          <li><a href="../../Pesquisas/CES/PesquisaCes.php">CES</a></li>
+        </ul>
+      </li>
+      <li>
+        
+        <li>
+            <div class="icon-link">
+               <a href="logout.php">
+                <i class="fa-solid fa-arrow-right-from-bracket"> </i>
+                   <span class="link_name">Sair</span>
+               </a>
+           
+           </div>
+         </li>
+         <li>
+  </div>
+  <section class="home-section">
+    <div class="home-content">
+      <i class='bx bx-menu' ></i>
+    </div>
+  </section>
+  <script>
+  let arrow = document.querySelectorAll(".arrow");
+  for (var i = 0; i < arrow.length; i++) {
+    arrow[i].addEventListener("click", (e)=>{
+   let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+   arrowParent.classList.toggle("showMenu");
+    });
+  }
+  let sidebar = document.querySelector(".sidebar");
+  let sidebarBtn = document.querySelector(".bx-menu");
+  console.log(sidebarBtn);
+  sidebarBtn.addEventListener("click", ()=>{
+    sidebar.classList.toggle("close");
+  });
+  </script>
 </body>
 </html>
